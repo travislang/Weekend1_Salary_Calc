96820b3ab048efd2e8590effb0478d4fa70c3b0b
Travis Lang <tlang50@icloud.com>
2018-11-09 15:11:57 -0600

    add new employee class anf function to create employee from inputs

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,38 @@
 $( document ).ready( readyNow );
 
+//create employee class
+class Employee{
+    constructor( first, last, id, title, salary ){
+        this.firstName = first;
+        this.lastName = last;
+        this.id = id;
+        this.title = title;
+        this.salary = salary;
+    }// end constructor
+}// end Employee class
+
+//setup employee array
+const employees = [];
+
 function readyNow( ){
-    console.log( 'working')
+    $( '#submitEmployeeBtn' ).on( 'click', newEmployee );
 }
+
+//create new employee from input fields
+function newEmployee( ){
+    let person = new Employee(
+        $( '#employeeFirstIn' ).val( ),
+        $( '#employeeLastIn' ).val( ),
+        $( '#employeeIdIn' ).val( ),
+        $( '#employeeTitleIn' ).val( ),
+        $( '#employeeSalaryIn' ).val( ),
+    );
+    //push new employee into array
+    employees.push( person );
+    //clear out input vals
+    $('#employeeFirstIn').val('');
+    $('#employeeLastIn').val('');
+    $('#employeeIdIn').val('');
+    $('#employeeTitleIn').val('');
+    $('#employeeSalaryIn').val('');
+}// end newEmployee function
